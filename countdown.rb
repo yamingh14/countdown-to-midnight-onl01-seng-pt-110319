e1a7ee860e2d11e5900254d46198fee5d988f024
dandy-bookmark-3637 <yamin.gh@gmail.com>
2019-11-14 15:51:02 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,18 @@
 def countdown(integer)
-  integer = 5
   while integer > 0
-    integer -= 1
-    puts "#{integer} seconds"
-sleep(1)
+      puts "#{integer} SECOND(S)!"
+        integer -= 1
   end
-    puts "HAPPY NEW YEAR!"
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+  while integer > 0
+      puts "#{integer} SECOND(S)!"
+        integer -= 1
+        sleep(1)
+  end
+    return "HAPPY NEW YEAR!"
 end
 #
 # def countdown_with_sleep
