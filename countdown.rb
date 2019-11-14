7e4a3b30b9beade7e750133f93578717bce6cdd3
dandy-bookmark-3637 <yamin.gh@gmail.com>
2019-11-14 09:09:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
-# def countdown(integer)
-#   integer = 5
-#   while integer > 0
-#     integer -= 1
-#     puts "#{integer} seconds"
-#   end
-#   puts "HAPPY NEW YEAR!"
-# end
+def countdown(integer)
+  integer = 5
+  while integer > 0
+    integer -= 1
+    puts "#{integer} seconds"
+  end
+  puts "HAPPY NEW YEAR!"
+end
 
 def countdown_with_sleep
     sleep(1)
